9ae448858ff3a2b0f5217f292cde9666a19c657f
fsbmat-ufv <fernando.bastos@ufv.br>
2023-11-29 18:05:20 +0000

    Cloud

--- Graficos.R
+++ Graficos.R
@@ -1,10 +1,151 @@
-rm(list=ls())
+rm(list = ls())
 cat("\014")
-library(esquisse)
-library(stringr)
-library(tidyverse)
-library(htmlTable)
-df <- readRDS("censo.Rds")
+#library("data.table")
+library("tidyverse")
+library("googlesheets4")
+library("googledrive")
+library("lubridate")
+
+# Google sheets authentification -----------------------------------------------
+options(gargle_oauth_cache = ".secrets")
+#drive_auth(cache = ".secrets", email = "darah.moreira@ufv.br")
+drive_auth(cache = ".secrets", email = "fernando.bastos@ufv.br")
+gs4_auth(token = drive_token())
+arq <- "https://docs.google.com/spreadsheets/d/12Eij3jGBKshx01hStY0odE7RXdzY-lWlI2X7_PQEp3A/edit?usp=sharing"
+df <- read_sheet(arq)
+# ------------------------------------------------------------------------------
+
+names(df) <- c("DataHora",
+               "Residencia",
+               "Estado",
+               "PrimeiraVez",
+               "Facilidade",
+               "Curso",
+               "AnoInicio",
+               "Semestre",
+               "Assistencia",
+               "Idade",
+               "HorasSono",
+               "EstudoAntecipado",
+               "HorasEstudo",
+               "Creditos",
+               "InteresseAreaAcademica",
+               "ResultadoEST",
+               "SalarioHoje",
+               "SalarioPosFormado")
+df <- tidyr::separate(df, DataHora, c("Data", "Hora"), sep = " ")
+names(df) <- abjutils::rm_accent(names(df))
+names(df) <- str_to_upper(names(df))
+df$RESIDENCIA            <- abjutils::rm_accent(df$RESIDENCIA            ) 
+df$ESTADO                <- abjutils::rm_accent(df$ESTADO                )
+df$PRIMEIRAVEZ           <- abjutils::rm_accent(df$PRIMEIRAVEZ           ) 
+df$FACILIDADE            <- abjutils::rm_accent(df$FACILIDADE            ) 
+df$CURSO                 <- abjutils::rm_accent(df$CURSO                 ) 
+df$ANOINICIO             <- abjutils::rm_accent(df$ANOINICIO             )
+df$SEMESTRE              <- abjutils::rm_accent(df$SEMESTRE              ) 
+df$ASSISTENCIA           <- abjutils::rm_accent(df$ASSISTENCIA           ) 
+df$IDADE                 <- abjutils::rm_accent(df$IDADE                 ) 
+df$HORASSONO             <- abjutils::rm_accent(df$HORASSONO             )
+df$ESTUDOANTECIPADO      <- abjutils::rm_accent(df$ESTUDOANTECIPADO      ) 
+df$HORASESTUDO           <- abjutils::rm_accent(df$HORASESTUDO           ) 
+df$CREDITOS              <- abjutils::rm_accent(df$CREDITOS              ) 
+df$INTERESSEAREAACADEMICA<- abjutils::rm_accent(df$INTERESSEAREAACADEMICA)
+df$RESULTADOEST          <- abjutils::rm_accent(df$RESULTADOEST)
+
+df$RESIDENCIA            <- str_to_upper(df$RESIDENCIA            )
+df$ESTADO                <- str_to_upper(df$ESTADO                ) 
+df$PRIMEIRAVEZ           <- str_to_upper(df$PRIMEIRAVEZ           ) 
+df$FACILIDADE            <- str_to_upper(df$FACILIDADE            )
+df$CURSO                 <- str_to_upper(df$CURSO                 ) 
+df$ANOINICIO             <- str_to_upper(df$ANOINICIO             ) 
+df$SEMESTRE              <- str_to_upper(df$SEMESTRE              )
+df$ASSISTENCIA           <- str_to_upper(df$ASSISTENCIA           )
+df$IDADE                 <- str_to_upper(df$IDADE                 ) 
+df$HORASSONO             <- str_to_upper(df$HORASSONO             )
+df$ESTUDOANTECIPADO      <- str_to_upper(df$ESTUDOANTECIPADO      ) 
+df$HORASESTUDO           <- str_to_upper(df$HORASESTUDO           ) 
+df$CREDITOS              <- str_to_upper(df$CREDITOS              )
+df$INTERESSEAREAACADEMICA<- str_to_upper(df$INTERESSEAREAACADEMICA) 
+df$RESULTADOEST          <- str_to_upper(df$RESULTADOEST          )
+df$TURMA <- "EST106"
+df[is.na(df)] <- 0
+
+##########----------------------------------------------------------------------
+#####Distribuicao de Frequencia - RESIDENCIA
+##########----------------------------------------------------------------------
+tab1 <- df %>% group_by(RESIDENCIA) %>% summarise(Freq=n(), .groups = 'drop' )
+tab1$FreqR <- round(tab1$Freq/sum(tab1$Freq), digits = 2)
+tab1$FreqACM <- cumsum(tab1$Freq)
+tab1$FreqRACM <- cumsum(tab1$FreqR)
+Total <- c("Total", sum(tab1$Freq), sum(tab1$FreqR), "-", "-")
+tab1 <- rbind(tab1, Total)
+
+tab1%>% 
+  addHtmlTableStyle(col.rgroup = c("none", "#F9FAF0"),
+                    col.columns = c("none", "#F1F0FA")) %>% 
+  htmlTable(total = TRUE, rnames = FALSE,
+            caption = "Tabela de Frequencia")
+
+###Correcao da coluna RESIDENCIA
+df <- df %>% mutate(RESIDENCIA=ifelse(as.character(RESIDENCIA)=="BH","BELO HORIZONTE",as.character(RESIDENCIA)))
+df <- df %>% mutate(RESIDENCIA=ifelse(as.character(RESIDENCIA)=="TEXAS","TEIXEIRAS",as.character(RESIDENCIA)))
+df <- df %>% mutate(RESIDENCIA=ifelse(as.character(RESIDENCIA)=="TEIXIERAS","TEIXEIRAS",as.character(RESIDENCIA)))
+df <- df %>% mutate(RESIDENCIA=ifelse(as.character(RESIDENCIA)=="VICOSA MG","VICOSA",as.character(RESIDENCIA)))
+
+#Tabela Corrigida
+
+tab1 <- df %>% group_by(RESIDENCIA) %>% summarise(Freq=n(), .groups = 'drop' )
+tab1$FreqR <- round(tab1$Freq/sum(tab1$Freq), digits = 2)
+tab1$FreqACM <- cumsum(tab1$Freq)
+tab1$FreqRACM <- cumsum(tab1$FreqR)
+Total <- c("Total", sum(tab1$Freq), sum(tab1$FreqR), "-", "-")
+tab1 <- rbind(tab1, Total)
+
+tab1%>% 
+  addHtmlTableStyle(col.rgroup = c("none", "#F9FAF0"),
+                    col.columns = c("none", "#F1F0FA")) %>% 
+  htmlTable(total = TRUE, rnames = FALSE,
+            caption = "Tabela de Frequencia")
+
+#Grafico de barra
+# %>% ctrl+shift+m printa o comando piple
+plot <- df %>% 
+  group_by(RESIDENCIA) %>% 
+  summarise(count=n(), .groups = 'drop') %>% 
+  ggplot(aes(reorder(RESIDENCIA, count), 
+             text = paste("Curso: ", 
+                          RESIDENCIA, "<br>",
+                          "Número de estudantes: ", 
+                          count), 
+             count, 
+             fill=RESIDENCIA)) + 
+  geom_col(position = "dodge", show.legend = F) + 
+  geom_text(aes(label = count), nudge_y = 1, size=5) +
+  coord_flip() + xlab("Residência") + ylab("Nº de Estudantes")
+
+plot %>% plotly::ggplotly(tooltip = "text") %>% 
+  plotly::layout(showlegend=FALSE)
+
+
+##########----------------------------------------------------------------------
+#####Distribuicao de Frequencia - ESTADO
+##########----------------------------------------------------------------------
+tab1 <- df %>% group_by(ESTADO) %>% summarise(Freq=n(), .groups = 'drop' )
+tab1$FreqR <- round(tab1$Freq/sum(tab1$Freq), digits = 2)
+tab1$FreqACM <- cumsum(tab1$Freq)
+tab1$FreqRACM <- cumsum(tab1$FreqR)
+Total <- c("Total", sum(tab1$Freq), sum(tab1$FreqR), "-", "-")
+tab1 <- rbind(tab1, Total)
+
+tab1%>% 
+  addHtmlTableStyle(col.rgroup = c("none", "#F9FAF0"),
+                    col.columns = c("none", "#F1F0FA")) %>% 
+  htmlTable(total = TRUE, rnames = FALSE,
+            caption = "Tabela de Frequencia")
+
+##########----------------------------------------------------------------------
+#####Distribuicao de Frequencia - CURSO
+##########----------------------------------------------------------------------
 tab1 <- df %>% group_by(CURSO) %>% summarise(Freq=n(), .groups = 'drop' )
 tab1$FreqR <- round(tab1$Freq/sum(tab1$Freq), digits = 2)
 tab1$FreqACM <- cumsum(tab1$Freq)
@@ -17,6 +158,46 @@ tab1%>%
                     col.columns = c("none", "#F1F0FA")) %>% 
   htmlTable(total = TRUE, rnames = FALSE,
             caption = "Tabela de Frequencia")
+
+##########----------------------------------------------------------------------
+#####Distribuicao de Frequencia - ANOINICIO
+##########----------------------------------------------------------------------
+tab1 <- df %>% group_by(ANOINICIO) %>% summarise(Freq=n(), .groups = 'drop' )
+tab1$FreqR <- round(tab1$Freq/sum(tab1$Freq), digits = 2)
+tab1$FreqACM <- cumsum(tab1$Freq)
+tab1$FreqRACM <- cumsum(tab1$FreqR)
+Total <- c("Total", sum(tab1$Freq), sum(tab1$FreqR), "-", "-")
+tab1 <- rbind(tab1, Total)
+
+tab1%>% 
+  addHtmlTableStyle(col.rgroup = c("none", "#F9FAF0"),
+                    col.columns = c("none", "#F1F0FA")) %>% 
+  htmlTable(total = TRUE, rnames = FALSE,
+            caption = "Tabela de Frequencia")
+
+
+##########----------------------------------------------------------------------
+#####Distribuicao de Frequencia - SEMESTRE
+##########----------------------------------------------------------------------
+tab1 <- df %>% group_by(SEMESTRE) %>% summarise(Freq=n(), .groups = 'drop' )
+tab1$FreqR <- round(tab1$Freq/sum(tab1$Freq), digits = 2)
+tab1$FreqACM <- cumsum(tab1$Freq)
+tab1$FreqRACM <- cumsum(tab1$FreqR)
+Total <- c("Total", sum(tab1$Freq), sum(tab1$FreqR), "-", "-")
+tab1 <- rbind(tab1, Total)
+
+tab1%>% 
+  addHtmlTableStyle(col.rgroup = c("none", "#F9FAF0"),
+                    col.columns = c("none", "#F1F0FA")) %>% 
+  htmlTable(total = TRUE, rnames = FALSE,
+            caption = "Tabela de Frequencia")
+
+str(df$SEMESTRE)
+df$SEMESTRE <- as.numeric(as.character(df$SEMESTRE))
+df[is.na(df)] <- 0
+
+
+
 
 df1 <- df %>% group_by(CURSO) %>% summarise(count=n(), .groups = 'drop' )
 barplot(height=df1$count, names=df1$CURSO, col="#69b3a2")
